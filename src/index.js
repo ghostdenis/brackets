0c58be52ea52ebd8ddb2a9ddc6663c406546a88b
ghostdenis <pr.denis.klimov@gmail.com>
2022-07-15 22:56:19 +0400

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBrackets = [];
+  let closedBrackets = [];
+  for (let i in bracketsConfig) {
+    openedBrackets.push(bracketsConfig[i][0]);
+    closedBrackets.push(bracketsConfig[i][1]);
+  }
+  let order = [];
+  for (let char of str) {
+    if (char === order[order.length - 1]) {
+      order.pop();
+      continue;
+    }
+    if (openedBrackets.find((o) => char == o)) {
+      order.push(closedBrackets[openedBrackets.findIndex((o) => char == o)]);
+      continue;
+    }
+    return false;
+  }
+  if (order.length) {
+    return false;
+  } else {
+    return true;
+  }
 }
